db21dcefc2c2e518a44383ad01f9ebff3c952a18
jkbpmsystem <pmsystembackend@gmail.com>
2026-04-01 09:45:30 +0800

    Update asset.js

--- js/asset.js
+++ js/asset.js
@@ -708,3 +708,12 @@ document.addEventListener("click", function(e){
   }
 
 });
+
+function formatDate(dateStr){
+  if(!dateStr) return "-";
+  const d = new Date(dateStr);
+  const day = String(d.getDate()).padStart(2,"0");
+  const month = String(d.getMonth()+1).padStart(2,"0");
+  const year = d.getFullYear();
+  return `${day}/${month}/${year}`;
+}
