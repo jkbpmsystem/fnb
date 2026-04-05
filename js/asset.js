7eeb9675eb34367b4d93d1f941b3464416d92f75
jkbpmsystem <pmsystembackend@gmail.com>
2026-04-05 17:52:15 +0800

    Update asset.js

--- js/asset.js
+++ js/asset.js
@@ -114,21 +114,20 @@ function renderTable(){
 
     console.log("SAMPLE OBJECT:", a);
 
-  html += `
-    <tr>
-      <td>${a.id}</td>
-      <td>${a.assetNo}</td>
-      <td>${a.equipmentName}</td>
-      <td>${a.typeCode}</td>
-      <td>${a.discipline}</td>
-      <td>${a.manufacture}</td>
-      <td>${a.model}</td>
-      <td>${formatDate(a.startDate)}</td>
-      <td>${formatDate(a.endDate)}</td>
-    </tr>
-  `;
+    html += `
+      <tr>
+        <td>${a.id}</td>
+        <td>${a.assetNo}</td>
+        <td>${a.equipmentName}</td>
+        <td>${a.typeCode}</td>
+        <td>${a.discipline}</td>
+        <td>${a.codeLocation}</td> <!-- 🔥 BETUL -->
+        <td>${formatDate(a.startDate)}</td>
+        <td>${formatDate(a.endDate)}</td>
+      </tr>
+    `;
 
-});
+  });
 
   tbody.innerHTML = html;
 }
