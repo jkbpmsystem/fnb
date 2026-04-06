d7a8ce1a762dbd0384b13dd22ff2668a6ad2e968
jkbpmsystem <pmsystembackend@gmail.com>
2026-04-06 09:48:21 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,8 +2,8 @@
 // API CONFIG
 // =====================
 const API = {
-  BASE: "https://script.google.com/macros/s/AKfycbyLxPzgzonubQGqgRoLqsuz6EQLj2JAEcTVC2TCFdkPG9CMI6cZ1iHuyTm1ui4QBIlRxg/exec"
-  "https://script.google.com/macros/s/AKfycbwHDAybRqO3zs6SXSaP3wQcdkNH9bU6v2QAGNy2yKT2GqfRRfcOczkCCI94oWxZEVcbPw/exec",
+  BASE: "https://script.google.com/macros/s/AKfycbyLxPzgzonubQGqgRoLqsuz6EQLj2JAEcTVC2TCFdkPG9CMI6cZ1iHuyTm1ui4QBIlRxg/exec",
+  LOGIN: "https://script.google.com/macros/s/AKfycbwHDAybRqO3zs6SXSaP3wQcdkNH9bU6v2QAGNy2yKT2GqfRRfcOczkCCI94oWxZEVcbPw/exec"
  };
 
 // =====================
